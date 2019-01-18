47ae63dfa29d7b105c2ea1761ed79f0ef327ee4d
Moore Dagogo Hart <mooredh99@gmail.com>
2019-01-18 21:52:04 +0100

    test on android

--- js/url.js
+++ js/url.js
@@ -1,6 +1,10 @@
 var android = /(android)/i.test(navigator.userAgent);
 var iOS = /(iPhone|iPad|iPod)/g.test(navigator.userAgent);
 
+alert(navigator.userAgent)
+alert(android)
+alert(ios)
+
 if (android && !iOS) {
     document.getElementById('main').innerHTML = ""
     window.location.href = "https://play.google.com/store/apps/details?id=com.colormatch"
