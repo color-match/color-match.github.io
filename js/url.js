9bb3d8caf48c4b50365a5464483b87c70cef4648
Moore Dagogo Hart <mooredh99@gmail.com>
2019-01-18 23:32:15 +0100

    ios test 5

--- js/url.js
+++ js/url.js
@@ -1,13 +1,13 @@
-var android = /(android)/i.test(navigator.userAgent);
-var iOS = /(iPhone|iPad|iPod)/g.test(navigator.userAgent);
+// var android = /(android)/i.test(navigator.userAgent);
+// var iOS = /(iPhone|iPad|iPod)/g.test(navigator.userAgent);
 
-alert(navigator.userAgent)
-alert(iOS)
+// alert(navigator.userAgent)
+// alert(iOS)
 
-if (android && !iOS) {
-    window.location.href = "https://play.google.com/store/apps/details?id=com.colormatch"
-}
+// if (android && !iOS) {
+//     window.location.href = "https://play.google.com/store/apps/details?id=com.colormatch"
+// }
 
-else if (iOS && !android) {
-    window.location.href = "https://facebook.com"
-}
+// else if (iOS && !android) {
+//     window.location.href = "https://facebook.com"
+// }
