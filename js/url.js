a5fa16188112f013cfe1503ecaef739721c4f3a8
Moore Dagogo Hart <mooredh99@gmail.com>
2019-01-18 22:02:06 +0100

    fixed i think

--- js/url.js
+++ js/url.js
@@ -1,16 +1,10 @@
 var android = /(android)/i.test(navigator.userAgent);
 var iOS = /(iPhone|iPad|iPod)/g.test(navigator.userAgent);
 
-alert(navigator.userAgent)
-alert(android)
-alert(iOS)
-
 if (android && !iOS) {
-    document.getElementById('main').innerHTML = ""
     window.location.href = "https://play.google.com/store/apps/details?id=com.colormatch"
 }
 
 else {
-    document.getElementById('main').innerHTML = ""
     window.location.href = ""
 }
