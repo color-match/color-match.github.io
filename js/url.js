53e1cfc9013ead4340ae84444af4d311f32862c2
Moore Dagogo Hart <mooredh99@gmail.com>
2019-01-31 22:12:08 +0100

    Added terms of use

--- js/url.js
+++ js/url.js
@@ -1,13 +1,10 @@
 var android = /(android)/i.test(navigator.userAgent);
 var iOS = /(iPhone|iPad|iPod)/g.test(navigator.userAgent);
 
-alert(navigator.userAgent)
-alert(iOS)
-
 if (android && !iOS) {
     window.location.href = "https://play.google.com/store/apps/details?id=com.colormatch"
 }
 
 else if (iOS && !android) {
-    window.location.href = "https://facebook.com"
+    window.location.href = "https://mooredh.github.io/color-match-support"
 }
